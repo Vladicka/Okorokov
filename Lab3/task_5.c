a1acbfc7e23746631a8166e087010074cb79e82a
Vladicka <vladicka7874@gmail.com>
2017-03-08 19:17:15 +0500

    Create task_5.c

---
+++ Lab3/task_5.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int a = 0;
+
+void *mythread(void *dummy) {
+    pthread_t mythid;
+    mythid = pthread_self();
+    a = a+1;
+    printf("Thread %d, Calculation result = %d\n", mythid, a);
+    return NULL;
+}
+
+void *unlimited_thread_works(void *archer){
+    pthread_t mythid;
+    mythid=pthread_self();
+    a=a+1;
+    printf("Thread %d, UNLIMITED THREAD WORKS, Calculation result = %d\n", mythid, a);
+    return NULL;
+}
+
+int main(){
+    pthread_t thid, mythid, thid2;
+    int result;
+    result = pthread_create(&thid, (pthread_attr_t *)NULL, mythread, NULL);
+    if(result != 0){
+        printf ("Error on thread create, return value = %d\n", result);
+        exit(-1);
+    }
+    printf("Thread created, thid = %d\n", thid);
+    result = pthread_create(&thid2, (pthread_attr_t *)NULL, unlimited_thread_works, NULL);
+    if(result != 0){
+        printf ("Error on thread create, return value = %d\n", result);
+        exit(-1);
+    }
+    printf("Thread created, thid = %d\n", thid2);
+    mythid = pthread_self();
+    a = a+1;
+    printf("Thread %d, Calculation result = %d\n", mythid, a);
+    pthread_join(thid, (void **)NULL);
+    pthread_join(thid2, (void **)NULL);
+    return 0;
+}
