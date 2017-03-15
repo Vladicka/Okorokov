9b61a397e7ded079e3871271764114965483997a
Vladicka <vladicka7874@gmail.com>
2017-03-16 02:20:52 +0500

    Create task_2_2.c

---
+++ Lab_5/task_2_2.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define LAST_MESSAGE 255
+
+int main() {
+    int msqid;
+    char pathname[] = "05_1a.c";
+    key_t key;
+    int len, maxlen;
+    struct mymsgbuf{
+        long mtype;
+        struct{
+            int i_info;
+            double d_info;
+        }content;
+    } mybuf;
+    if((key = ftok(pathname,0)) < 0){
+        printf("Can\'t generate key\n");
+        exit(-1);
+    }
+    if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0){
+        printf("Can\'t get msqid\n");
+        exit(-1);
+    }
+    while(1){
+        maxlen = 16;
+        if (len = msgrcv(msqid, (struct msgbuf *) &mybuf, maxlen, 0, 0) < 0){
+            printf("Can\'t receive message from queue\n");
+            exit(-1);
+        }
+        if (mybuf.mtype == LAST_MESSAGE){
+            msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+            exit(0);
+        }
+        printf("message type = %ld, i_info = %d, d_info= %f\n", mybuf.mtype, mybuf.content.i_info, mybuf.content.d_info);
+    }
+    return 0;
+}
