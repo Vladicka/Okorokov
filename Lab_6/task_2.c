5d51a2ab5d8218d0b7ddb0dcfd6c271bc707a819
Vladicka <vladicka7874@gmail.com>
2017-03-20 20:31:05 +0500

    Create task_2.c

---
+++ Lab_6/task_2.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <iostream>
+#include <dirent.h>
+
+using namespace std;
+
+int main(){
+    DIR *curr_dir;
+    struct dirent *curr_file;
+    (void)umask(0);
+    if((curr_dir = opendir("/root")) < 0){
+        cout << "Can't open directory.\n";
+        exit(-1);
+    }
+    curr_file=readdir(curr_dir);
+    if (curr_file==NULL){
+        cout << "Can't get info about file in directory.\n";
+        exit(-1);
+    }
+    while(curr_file!=NULL){
+        cout << curr_file->d_name << "\n";
+        curr_file=readdir(curr_dir);
+    }
+    if (closedir(curr_dir)<0){
+        cout << "Can't close directory.\n";
+    }
+    return 0;
+}
