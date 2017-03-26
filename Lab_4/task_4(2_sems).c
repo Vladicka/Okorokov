6f2ca0a6f9632dccad9a70bf9d7ab45cf7c3c7e9
Vladicka <vladicka7874@gmail.com>
2017-03-26 22:44:54 +0500

    Create task_4(2_sems).c

---
+++ Lab_4/task_4(2_sems).c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int fd[2], result, res, semid, i;
+    char msg[24];
+    char pathname[] = "temp4";
+    string prnt_msg, ch_msg, num;
+    key_t sem_key;
+    struct sembuf mysem[2];
+    if(pipe(fd) < 0){
+        cout << "Can't create pipe\n";
+        exit(-1);
+    }
+    if((sem_key = ftok(pathname,0)) < 0){
+        cout << "Can't generate sem key\n";
+        exit(-1);
+    }
+    if((semid = semget(sem_key, 1, 0666 | IPC_CREAT)) < 0){
+        cout << "Can't get semid\n";
+        exit(-1);
+    }
+    mysem[0].sem_flg=0;
+    mysem[0].sem_num=0;
+    mysem[1].sem_flg=0;
+    mysem[1].sem_num=0;
+    result = fork();
+    if(result <0){
+        cout << "Can't fork child\n";
+        exit(-1);
+    }
+    else if (result > 0) {
+        for (i=0;i<7;i++){
+            num=i+1+'0';
+            prnt_msg="Message from parent №" + num;
+            res=write(fd[1], prnt_msg.c_str() , 24);
+            if (res!=24){
+                cout << "Can't send message\n";
+                exit(1);
+            }
+            mysem[1].sem_op=1;
+            semop(semid, &mysem[1], 1);
+            mysem[0].sem_op=-1;
+            semop(semid, &mysem[0], 1);
+            res=read(fd[0], msg, 23);
+            cout << "Message №" << i+1 << " received from child!\nMessage content: " << msg << "\n\n";
+        }
+    }
+    else {
+        for (i=0; i<7; i++){
+            mysem[1].sem_op=-1;
+            semop(semid, &mysem[1], 1);
+            res=read(fd[0], msg, 24);
+            cout << "Message №" << i+1 << " received from parent!\nMessage content: " << msg << "\n\n";
+            num=i+1+'0';
+            ch_msg="Message from child №" + num;
+            res=write(fd[1], ch_msg.c_str(), 23);
+            if (res!=23){
+                cout << "Can't send message\n";
+                exit(1);
+            }
+            mysem[0].sem_op=1;
+            semop(semid, &mysem[0], 1);
+        }
+    }
+    semctl(semid, IPC_RMID, 0);
+    return 0;
+}
